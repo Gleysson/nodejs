36067f58f010885680b050121e6fde56dd194f16
Gleysson Rocha <gleyssonrocha@nucleus.eti.br>
2020-11-04 13:37:11 -0300

    Autenticate Controller Example

---
+++ src/controllers/v1/User/authenticate.js
@@ -0,0 +1,40 @@
+const { validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken')
+
+module.exports = function(app){
+
+    const secretKey = app.src.config.server.SECRET_KEY
+
+    this.index = function(req, res, next){
+
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const payload = {
+            id: 1,
+            name: 'Gleysson Rocha'
+        }
+
+        const token = jwt.sign(payload, secretKey, {
+            expiresIn: '5m' 
+        })
+        
+        const verify = jwt.verify(token, secretKey, function(err, decoded){
+            return res.send({
+                err, decoded
+            })
+        })
+
+        return res.send({
+            token,
+            verify
+        })
+
+    }
+
+
+    return this;
+}
